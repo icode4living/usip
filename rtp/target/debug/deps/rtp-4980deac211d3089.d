d2e7ed165ff5971faee42344003b5ce95f977411
samuelsamafolabi@outlook.com <samuelsamafolabi@outlook.com>
2023-08-17 16:56:52 +0100

    host name parsing

---
+++ rtp/target/debug/deps/rtp-4980deac211d3089.d
@@ -0,0 +1,5 @@
+C:\Users\IT DEPARTMENT\Music\proj\usip\rtp\target\debug\deps\rtp-4980deac211d3089.rmeta: src\lib.rs
+
+C:\Users\IT DEPARTMENT\Music\proj\usip\rtp\target\debug\deps\rtp-4980deac211d3089.d: src\lib.rs
+
+src\lib.rs:
---
+++ sip/target/debug/build/memchr-7c5434ada9f4b665/build_script_build-7c5434ada9f4b665.d
@@ -0,0 +1,5 @@
+C:\Users\IT DEPARTMENT\Music\proj\usip\sip\target\debug\build\memchr-7c5434ada9f4b665\build_script_build-7c5434ada9f4b665.exe: C:\Users\IT\ DEPARTMENT\.cargo\registry\src\github.com-1ecc6299db9ec823\memchr-2.5.0\build.rs
+
+C:\Users\IT DEPARTMENT\Music\proj\usip\sip\target\debug\build\memchr-7c5434ada9f4b665\build_script_build-7c5434ada9f4b665.d: C:\Users\IT\ DEPARTMENT\.cargo\registry\src\github.com-1ecc6299db9ec823\memchr-2.5.0\build.rs
+
+C:\Users\IT\ DEPARTMENT\.cargo\registry\src\github.com-1ecc6299db9ec823\memchr-2.5.0\build.rs:
