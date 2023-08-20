b691e113440097a8a0dd00add73eee27ff5ad45c
icode4living <samuelsamafolabi@outlook.com>
2023-08-20 14:11:41 +0100

    Host parsing

--- sip/src/header/request.rs
+++ sip/src/header/request.rs
@@ -12,23 +12,28 @@ use nom::{
 //SIP methods
 #[derive(Debug,PartialEq)]
 pub enum Method{
-Invite,
-Ack,
-Options,
-Cancel,
-Bye,
-Register
+ INVITE,
+ ACK,
+ OPTIONS,
+CANCEL,
+BYE,
+REGISTER
+}
+//The host cant either be IP or a domain name
+pub enum Host{
+    HOST(String)
+    IP([u8;4])
 }
 
 impl From<&str> for Method{
     fn from(i:&str)->Self{
         match  i {
-           "%x49.4E.56.49.54.45 ;" | "INVITE" => Method::Invite,
-            "%x41.43.4B ;" | "ACK" =>Method::Ack,
-            "%x4F.50.54.49.4F.4E.53 ;"|"OPTIONS" => Method::Options,
-            "%x42.59.45 ;" | "BYE" => Method::Bye,
-            "%x43.41.4E.43.45.4C ;" | "CANCLE" =>  Method::Cancel,
-            "%x52.45.47.49.53.54.45.52 ;" |"REGISTER" => Method::Register,
+           "%x49.4E.56.49.54.45 ;" | "INVITE" => Method::INVITE,
+            "%x41.43.4B ;" | "ACK" =>Method::ACK,
+            "%x4F.50.54.49.4F.4E.53 ;"|"OPTIONS" => Method::OPTIONS,
+            "%x42.59.45 ;" | "BYE" => Method::BYE,
+            "%x43.41.4E.43.45.4C ;" | "CANCLE" =>  Method::CANCEL,
+            "%x52.45.47.49.53.54.45.52 ;" |"REGISTER" => Method::REGISTER,
             _ => unimplemented!("Unsupported method")
         }
     }
